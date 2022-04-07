d1d3daf5ae53d207b5b5fb377729968498eb8136
Thijs van Dijk <thijs3214@gmail.com>
2022-04-07 22:04:03 +0200

    Updated builder for linux to use n - 1 threads for compilation by default

--- builder_linux.sh
+++ builder_linux.sh
@@ -2,9 +2,11 @@ echo "------- Getting GLFW LIBRARY -------"
 git clone https://github.com/glfw/glfw.git
 echo "------- Building GLFW LIBRARY -------"
 
+NOFPROC=`expr $(nproc) - 1`
+
 mkdir glfw/build
 cmake -B ./glfw/build/ -S glfw/ -DBUILD_SHARED_LIBS=ON
-cmake --build glfw/build/ --config Release -j7
+cmake --build glfw/build/ --config Release -j$NOFPROC
 
 mkdir lib
 cp glfw/build/src/libglfw.* lib/
@@ -15,6 +17,6 @@ echo "------- Building Executable -------"
 
 mkdir build
 cmake -B build/ -S ./
-cmake --build build/ --config Release -j7
+cmake --build build/ --config Release -j$NOFPROC
 
 echo "Finished Building..."
