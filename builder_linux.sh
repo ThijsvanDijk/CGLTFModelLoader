f96112df8e5a307c87c1c2c061d4e851aecc92f6
Thijs van Dijk <thijs3214@gmail.com>
2022-04-08 02:10:10 +0200

    Updated builder first version GL loader

--- builder_linux.sh
+++ builder_linux.sh
@@ -1,18 +1,36 @@
-echo "------- Getting GLFW LIBRARY -------"
-git clone https://github.com/glfw/glfw.git
-echo "------- Building GLFW LIBRARY -------"
+echo "------- Setup -------"
 
+mkdir lib
 NOFPROC=`expr $(nproc) - 1`
+
+echo "------- Downloading Libs -------"
+
+mkdir submodules && cd submodules
+git clone https://github.com/glfw/glfw.git
+git clone https://github.com/nigels-com/glew.git
+
+echo "------- Building GLFW LIBRARY -------"
 
 mkdir glfw/build
 cmake -B ./glfw/build/ -S glfw/ -DBUILD_SHARED_LIBS=ON
 cmake --build glfw/build/ --config Release -j$NOFPROC
 
-mkdir lib
-cp glfw/build/src/libglfw.* lib/
-cp -r glfw/include/* include/
+cp glfw/build/src/libglfw.so* ../lib/
+cp -r glfw/include/* ../include/
 
 echo "------- Done Building GLFW LIBRARY -------"
+echo "------- Building GLEW LIBRARY -------"
+
+mkdir glew/build
+cmake -B ./glew/build/ -S glew/
+cmake --build glew/build/ -j$NOFPROC
+
+cp glew/build/lib/libglut.so* ../lib/
+cp -r glew/include/* ../include/
+
+echo "------- Done Building GLEW LIBRARY -------"
+cd ../
+# rm -r submodules
 echo "------- Building Executable -------"
 
 mkdir build
