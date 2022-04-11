97c0dab959085a502e2d63028806e9bfef7553f4
Thijs van Dijk <thijs3214@gmail.com>
2022-04-11 14:45:57 +0200

    Added cglm to include

--- src/main.c
+++ src/main.c
@@ -1,43 +1,48 @@
-#include <stdlib.h>
-#include <stdio.h>
+// #include <stdlib.h>
+// #include <stdio.h>
 
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
+// #include <glad/gl.h>
+// #include <GLFW/glfw3.h>
+#include <cglm/cglm.h>
 
 
-const GLuint WIDTH = 800, HEIGHT = 600;
+// const GLuint WIDTH = 800, HEIGHT = 600;
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
+// void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+//     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+//         glfwSetWindowShouldClose(window, GL_TRUE);
+// }
 
 int main(void) {
-    glfwInit();
+    // glfwInit();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GL with GLFW", NULL, NULL);
-    glfwMakeContextCurrent(window);
+    // GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GL with GLFW", NULL, NULL);
+    // glfwMakeContextCurrent(window);
 
-    glfwSetKeyCallback(window, key_callback);
+    // glfwSetKeyCallback(window, key_callback);
 
-    gladLoadGL(glfwGetProcAddress);
+    // gladLoadGL(glfwGetProcAddress);
 
-    glViewport(0, 0, WIDTH, HEIGHT);
+    // glViewport(0, 0, WIDTH, HEIGHT);
+    vec3 v1 = {0.6f, 7.2f, 9.61f};
+    vec3 v2 = {8.6f, 3.5f, 0.1f};
+    vec3 v3;
+    glm_vec3_cross(v1, v2, v3);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    // while (!glfwWindowShouldClose(window)) {
+    //     glfwPollEvents();
 
-        glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+    //     glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+    //     glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
-    }
+    //     glfwSwapBuffers(window);
+    // }
 
-    glfwTerminate();
+    // glfwTerminate();
 
     return 0;
 }
