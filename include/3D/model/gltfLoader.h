945cfb6afc334a3ef0e7bfd093333f12008ef839
Thijs van Dijk <thijs3214@gmail.com>
2022-06-05 21:17:49 +0200

    Added some commentary, preparing for branch

--- include/3D/model/gltfLoader.h
+++ include/3D/model/gltfLoader.h
@@ -2,7 +2,20 @@
 #include <cglm/cglm.h>
 #include <glad/glad.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+#define GLB_IDENTIFICATION_CODE 0x46546C67
+#define GLB_CHUNK_STRUCTURED_JSON 0x4E4F534A
+#define GLB_CHUNK_BINARY_BUFFER 0x004E4942
+#define GLB_CHUNK_BYTE_ALIGNMENT 4
+#define GLTF_VERSION 2
+
+
+/**
+ * @brief Vertices are made of:
+ * position vectors (float[3]),
+ * normal vectors (float[3]) 
+ */
 typedef struct {
     vec3 position;
     vec3 normal;
@@ -11,22 +24,86 @@ typedef struct {
     // vec3 bitangent;
 } Vertex;
 
+/**
+ * @brief Meshes are used to store vertex and index data loaded from 3d models 
+ */
 typedef struct {
     Vertex * vertices;
     unsigned int * indices;
 } Mesh;
 
+/**
+ * @brief Struct that helps convert GLTF type codes to OpenGL internal codes 
+ */
 typedef struct {
     unsigned int glType;
-    unsigned int gltfType;
-} componentType;
+    unsigned short gltfType;
+    unsigned short amountOfBits;
+} ComponentType;
 
+/**
+ * @brief Every GLTF file has a 12 bit header with some basic info:
+ * an identifier code, the version number and the length of the file (including header) 
+ */
 typedef struct {
+    unsigned int magic, version, length;
+} GLTFHeader;
+
+/**
+ * @brief Two chunks can exist in a GLB file, the JSON and the buffer. Both sections have
+ * a short header which specifies which kind this section is and what the length of the section is. 
+ */
+typedef struct {
+    unsigned int chunkLength, chunkType;
+} GLTFChunkHeader;
+
+/**
+ * @brief Is used to store information about a GLTF File. This is done so the reading of the data can be done
+ * as uninterrupted as possible. 
+ */
+typedef struct {
+    Allocator * allocator;
     char bin_buffer_uri[256];
+    const char * json_data;
+    unsigned int json_data_size;
     unsigned int vertex_count, vertex_position_data_offset, vertex_normal_data_offset, vertex_texcoord_data_offset;
-    unsigned int index_count, index_data_offset;
-    unsigned int vertex_position_type, vertex_normal_type, vertex_texcoord_type, index_type;
+    unsigned int index_count, index_data_offset, index_type;
+    unsigned int vertex_position_type, vertex_normal_type, vertex_texcoord_type;
+    unsigned int glb_size;
 } BufferInfo;
 
-unsigned char getGLTFModelInfo(const char * file, BufferInfo * data);
+/**
+ * @brief Convert a GLTF file into a optimised GLB format where data is stored exactly as OpenGL works with it,
+ * allowing possible future extension with DirectStorage like technologies, drastically reducing load times.
+ * 
+ * @param info The Bufferinfo that contains info about the model that needs to be converted
+ * @param mesh The mesh where the model data is stored
+ * @param filename The created GLB file is stored here
+ * @return char Returns 0 if succesful, if 0 is not returned: error
+ */
+char convertGLTFToGLB(BufferInfo * info, Mesh * mesh, const char * filename);
+
+/**
+ * @brief Gets info about a model before loading occurs
+ * 
+ * @param file File to get info from
+ * @param data Info struct to fill in the model info from file
+ * @return char 0 if succesful, < 0 if unsuccesful
+ */
+char getGLTFModelInfo(const char * file, BufferInfo * data);
+
+/**
+ * @brief Reads the vertices and indices from the buffer and stores these in result
+ * 
+ * @param buffer_data Info struct with info about the 3D model
+ * @param result The Mesh where the data is stored
+ * @return char 0 if succesful, < 0 if unsuccesful
+ */
 char loadGLTFBinaryData(BufferInfo * buffer_data, Mesh * result);
+
+/**
+ * @brief UNDER CONSTRUCTION! Will eventually read the optimised GLB file and store data in result
+ * 
+ * @param file File to read the data from
+ */
+void readGLB(const char * file);
--- src/modules/3D/model/gltfLoader.c
+++ src/modules/3D/model/gltfLoader.c
@@ -2,23 +2,58 @@
 
 #define NO_FREAD
 
-static const componentType types[] = {
-    {GL_BYTE, 5120},
-    {GL_UNSIGNED_BYTE, 5121},
-    {GL_SHORT, 5122},
-    {GL_UNSIGNED_SHORT, 5123},
-    {GL_UNSIGNED_INT, 5125},
-    {GL_FLOAT, 5126}
+static const ComponentType components[] = {
+    {GL_BYTE, 5120, 8},
+    {GL_UNSIGNED_BYTE, 5121, 8},
+    {GL_SHORT, 5122, 16},
+    {GL_UNSIGNED_SHORT, 5123, 16},
+    {GL_UNSIGNED_INT, 5125, 32},
+    {GL_FLOAT, 5126, 32}
 };
 
 unsigned int getGLComponentTypeFromGLTFType(unsigned int type){
-    for(unsigned int i = 0; i < sizeof(types)/sizeof(componentType); i++){
-        if(types[i].gltfType == type) return types[i].glType;
+    for(unsigned int i = 0; i < sizeof(components)/sizeof(ComponentType); i++){
+        if(components[i].gltfType == type) return components[i].glType;
     }
     return 0;
 }
 
-unsigned char getGLTFModelInfo(const char * filename, BufferInfo * data){
+char convertGLTFToGLB(BufferInfo * info, Mesh * mesh, const char * filename){
+    if(loadGLTFBinaryData(info, mesh) == -1) return -1;
+    int file_d = open(filename, O_CREAT | O_WRONLY | O_NONBLOCK | O_TRUNC);
+    unsigned char spaces[4] = "    "; // For padding
+    unsigned char zeroes[4] = "\0\0\0\0";
+    unsigned int buffer_length = info->index_count * sizeof(unsigned short) + info->vertex_count * sizeof(Vertex);
+
+    GLTFHeader header = {
+        .magic = GLB_IDENTIFICATION_CODE,
+        .version = GLTF_VERSION,
+        .length = info->glb_size
+    };
+    GLTFChunkHeader json_header = {
+        .chunkType = GLB_CHUNK_STRUCTURED_JSON,
+        .chunkLength = info->json_data_size + info->json_data_size % GLB_CHUNK_BYTE_ALIGNMENT
+    };
+    GLTFChunkHeader buffer_header = {
+        .chunkType = GLB_CHUNK_BINARY_BUFFER,
+        .chunkLength = buffer_length + buffer_length % GLB_CHUNK_BYTE_ALIGNMENT
+    };
+
+    write(file_d, &header, sizeof(GLTFHeader));
+    write(file_d, &json_header, sizeof(GLTFChunkHeader));
+    write(file_d, info->json_data, info->json_data_size);
+    write(file_d, spaces, info->json_data_size % GLB_CHUNK_BYTE_ALIGNMENT);
+    write(file_d, &buffer_header, sizeof(GLTFChunkHeader));
+    write(file_d, mesh->vertices, info->vertex_count * sizeof(Vertex));
+    write(file_d, mesh->indices, info->index_count * sizeof(unsigned short));
+    write(file_d, zeroes, buffer_length % GLB_CHUNK_BYTE_ALIGNMENT);
+
+    close(file_d);
+    ReleaseAllocator(info->allocator);
+    return 0;
+}
+
+char getGLTFModelInfo(const char * filename, BufferInfo * data){
     //---------- READING GLTF FILE ----------//
     FILE *file = fopen(filename, "rb");
     if(file == NULL){
@@ -36,42 +71,32 @@ unsigned char getGLTFModelInfo(const char * filename, BufferInfo * data){
     fclose(file);
 
     //---------- PARSING JSON ----------//
-    Allocator *A = NewAllocator();
-    Value *parsed_json = NewValue(A);
+    data->allocator = NewAllocator();
+    Value *parsed_json = NewValue(data->allocator);
     unsigned char ret = ParseFast(parsed_json, gltf_json);
     if(ret != 1){
-        fprintf(stderr, "ParseFast Fail!\n");
-        ReleaseAllocator(A);
         return -1;
     }
 
     //---------- GET ACCESSORS ----------//
     Value * meshes = ObjGet(parsed_json, "meshes");
     if(meshes == 0){
-        fprintf(stderr, "No mesh data found\n");
-        ReleaseAllocator(A);
         return -1;
     }
     Value * mesh_first_primitives = ArrayGet(ObjGet(ArrayGet(meshes, 0), "primitives"), 0);
+
     const int * index_accessor_index = GetInt(ObjGet(mesh_first_primitives, "indices"));
     if(index_accessor_index == 0){
-        fprintf(stderr, "No index data found\n");
-        ReleaseAllocator(A);
         return -1;
     }
+
     const int * vertex_position_accessor_index = GetInt(ObjGet(ObjGet(mesh_first_primitives, "attributes"), "POSITION"));
     if(vertex_position_accessor_index == 0){
-        fprintf(stderr, "No position data found for vertices\n");
-        ReleaseAllocator(A);
         return -1;
     }
 
     const int * vertex_normal_accessor_index = GetInt(ObjGet(ObjGet(mesh_first_primitives, "attributes"), "NORMAL"));
-    if(vertex_normal_accessor_index == 0){
-        fprintf(stderr, "No normal data found for vertices\n");
-        ReleaseAllocator(A);
-        return -1;
-    }      
+    if(vertex_normal_accessor_index == 0) return -1;  
 
     //---------- GET BUFFERVIEWS AND COUNTS ----------//
     Value * accessors = ObjGet(parsed_json, "accessors");
@@ -104,6 +129,10 @@ unsigned char getGLTFModelInfo(const char * filename, BufferInfo * data){
 
     const int * buffer_index = GetInt(ObjGet(index_bufferview, "buffer"));
 
+    data->glb_size = sizeof(Vertex) * data->vertex_count + sizeof(unsigned short) * data->index_count + 2 * sizeof(GLTFChunkHeader) + sizeof(GLTFHeader) + strlen(gltf_json) + strlen(gltf_json) % GLB_CHUNK_BYTE_ALIGNMENT;
+    data->json_data = Stringify(parsed_json);
+    data->json_data_size = strlen(data->json_data);
+
     //---------- GET BUFFERS URI ----------//
     Value * buffer = ArrayGet(ObjGet(parsed_json, "buffers"), *buffer_index);
     const char * uri = GetStr(ObjGet(buffer, "uri"));
@@ -111,8 +140,8 @@ unsigned char getGLTFModelInfo(const char * filename, BufferInfo * data){
     memset(data->bin_buffer_uri, 0, 256);
     strncpy(data->bin_buffer_uri, filename, (last_directory - filename) + 1);
     strncpy((last_directory - filename) + data->bin_buffer_uri + 1, uri, 256 - (last_directory - filename) - 1);
+    ObjDel(buffer, "uri");
 
-    ReleaseAllocator(A);
     return 0;
 }
 
@@ -123,7 +152,7 @@ char loadGLTFBinaryData(BufferInfo * buffer_data, Mesh * result){
         fprintf(stderr, "Failed to open file: %s\n", buffer_data->bin_buffer_uri);
         return -1;
     }
-    unsigned char loaded_data[2 * sizeof(vec3) * buffer_data->vertex_count];
+    unsigned char loaded_data[sizeof(Vertex) * buffer_data->vertex_count];
 
     lseek(file_d, buffer_data->vertex_position_data_offset, SEEK_SET);
     read(file_d, loaded_data, sizeof(vec3) * buffer_data->vertex_count);
