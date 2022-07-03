5d749c32e4b1921865b06da93e740a60dcf6d291
Thijs van Dijk <thijs3214@gmail.com>
2022-07-03 22:06:46 +0200

    Removed mistake from windows build script

--- build-windows.bat
+++ build-windows.bat
@@ -1,6 +1,4 @@
-mkdir build
-CC=C:\msys64\mingw64\bin\gcc.exe
-CXX=C:\msys64\mingw64\bin\g++.exe 
+mkdir build 
 cmake -G "MinGW Makefiles" -B build/ -S ./
 cmake --build build/ --config Release
 .\build\CGLTFModelLoader.exe
